8bacb5d55157fcabbc16289f10a6977c9314b331
0o0x0o0 <pyk120@126.com>
2022-05-27 21:37:59 +0800

    add

--- vue-project/src/components/PYK-component/booklist.vue
+++ vue-project/src/components/PYK-component/booklist.vue
@@ -1,46 +1,54 @@
 <template>
-  <el-table
-    :data="booktableData"
-    stripe
-    highlight-current-row
-    style="width: 100%"
-	  :default-sort = "{prop: 'rank', order: 'ascending'}"
-    height="750"
-  >
-    <el-table-column prop="rank" sortable width="20" > </el-table-column>
-    <el-table-column width="120">
-      <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>{{ scope.row.bookname }}</p>
-          <p>[{{ scope.row.author_country }}] {{ scope.row.author }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-button
-              size="middle"
-              @click="gotobooklink(scope.$index, scope.row)"
-            >
-              {{ scope.row.bookname }}
-            </el-button>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-    <el-table-column align="right">
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="gotopurchaselink(scope.$index, scope.row)"
-          >去购买</el-button
-        >
-      </template>
-    </el-table-column>
-  </el-table>
+    <el-table
+      :data="booktableData"
+      stripe
+      highlight-current-row
+      style="width: 100%"
+      :default-sort="{ prop: 'rank', order: 'ascending' }"
+       max-height="800"
+	   >
+      <el-table-column prop="rank" width="40">
+        <template slot-scope="scope">{{scope.row.rank}}.</template>
+      </el-table-column>
+      <el-table-column label="畅销图书榜" width="300">
+        <template slot-scope="scope">
+          <el-descriptions :column="2" size="mini" :colon="false">
+            <el-descriptions-item span="2" label="">
+              <el-link
+                :underline="false"
+                :href="scope.row.book_link"
+                style="font-size: 15px"
+              >
+                {{ scope.row.bookname | ellipsis }}
+              </el-link>
+            </el-descriptions-item>
+            <el-descriptions-item>
+              [{{ scope.row.author_country }}] {{ scope.row.author }}
+            </el-descriptions-item>
+          </el-descriptions>
+        </template>
+      </el-table-column>
+      <el-table-column width="100">
+        <template slot-scope="scope">
+          <el-button
+            size="middle"
+			type="text"
+			plain
+            @click="gotopurchaselink(scope.$index, scope.row)"
+            >去购买</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      booktableData: [],
+      booktableData: [
+        
+      ],
     };
   },
   methods: {
@@ -51,10 +59,19 @@ export default {
       this.$router.push(row.purchase_link);
     },
     download_booklist() {
-      this.$axios.get("api/book").then((res) => {
+      this.$axios.get("http://39.105.102.182:8080/api/book").then((res) => {
         console.log(res.data);
-        this.booktableData = res.data.data;
+        this.booktableData = res.data.booktableData;
       });
+    },
+  },
+  filters: {
+    ellipsis: function (value) {
+      if (!value) return "";
+      if (value.length > 30) {
+        return value.slice(0, 30) + "...";
+      }
+      return value;
     },
   },
   created() {
@@ -64,28 +81,55 @@ export default {
 </script>
 
 <!--
-			{
-				rank:'1',
-				bookname:'bookazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz',
-				author_country:'英',
-				author:'author_a',
-				book_link:'/',
-				purchase_link:'/'
-			},
-			{
-				rank:'2',
-				bookname:'bookb',
-				author_country:'英',
-				author:'author_b',
-				book_link:'/',
-				purchase_link:'/'
-			},
-			{
-				rank:'3',
-				bookname:'bookc',
-				author_country:'英',
-				author:'author_c',
-				book_link:'/',
-				purchase_link:'/'
-			},
+		{
+          rank: 1,
+          bookname: "bookazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+          author_country: "英",
+          author: "author_a",
+          book_link: "/",
+          purchase_link: "/",
+        },
+        {
+          rank:2,
+          bookname: "bookazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+          author_country: "英",
+          author: "author_a",
+          book_link: "/",
+          purchase_link: "/",
+        },{
+          rank: 3,
+          bookname: "bookazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+          author_country: "英",
+          author: "author_a",
+          book_link: "/",
+          purchase_link: "/",
+        },{
+          rank: 4,
+          bookname: "bookazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+          author_country: "英",
+          author: "author_a",
+          book_link: "/",
+          purchase_link: "/",
+        },{
+          rank: 5,
+          bookname: "bookazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+          author_country: "英",
+          author: "author_a",
+          book_link: "/",
+          purchase_link: "/",
+        },{
+          rank: 6,
+          bookname: "bookazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+          author_country: "英",
+          author: "author_a",
+          book_link: "/",
+          purchase_link: "/",
+        },{
+          rank: 7,
+          bookname: "bookazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+          author_country: "英",
+          author: "author_a",
+          book_link: "/",
+          purchase_link: "/",
+        },
 -->
--- vue-project/src/components/PYK-component/grouplist.vue
+++ vue-project/src/components/PYK-component/grouplist.vue
@@ -4,36 +4,40 @@
     stripe
     highlight-current-row
     style="width: 100%"
-	  :default-sort = "{prop: 'group_size', order: 'descending'}"
-    height="750"
+    :default-sort="{ prop: 'group_size', order: 'descending' }"
+    max-height="1000"
   >
     <el-table-column width="48">
       <template slot-scope="scope">
         <a :href="scope.row.group_link">
-          <img :src="scope.row.grouppicture_link" min-width="48" height="48" />
+          <img :src="scope.row.grouppicture_link" width="48" height="48" />
         </a>
       </template>
     </el-table-column>
-    <el-table-column width="160">
+    <el-table-column prop="group_size" label="值得加入的小组" width="350">
       <template slot-scope="scope">
-        <el-button
-          size="middle"
-          @click="gotogrouplink(scope.$index, scope.row)"
-        >
-          {{ scope.row.groupname }}
-        </el-button>
-      </template>
-    </el-table-column>
-    <el-table-column prop="group_size">
-      <template slot-scope="scope">
-        <p>{{ scope.row.group_size }}个成员</p>
-      </template>
-    </el-table-column>
-    <el-table-column>
-      <template slot-scope="scope">
-        <el-button size="mini" @click="gotogrouplink(scope.$index, scope.row)"
-          >+加入小组</el-button
-        >
+        <el-descriptions :column="2" size="mini" :colon="false">
+          <el-descriptions-item span="2" label="">
+            <el-link
+              :underline="false"
+              :href="scope.row.group_link"
+              style="font-size: 15px"
+            >
+              {{ scope.row.groupname | ellipsis }}
+            </el-link>
+          </el-descriptions-item>
+          <el-descriptions-item label="">
+            {{ scope.row.group_size }}个成员
+          </el-descriptions-item>
+          <el-descriptions-item label="">
+            <el-button
+              size="mini"
+              @click="gotogrouplink(scope.$index, scope.row)"
+            >
+              +加入小组
+            </el-button>
+          </el-descriptions-item>
+        </el-descriptions>
       </template>
     </el-table-column>
   </el-table>
@@ -43,7 +47,9 @@
 export default {
   data() {
     return {
-      grouptableData: [ ],
+      grouptableData: [
+        
+      ],
     };
   },
   methods: {
@@ -51,19 +57,38 @@ export default {
       this.$router.push(row.group_link);
     },
     download_grouplist() {
-      this.$axios.get("api/group").then((res) => {
+      this.$axios.get("http://39.105.102.182:8080/api/group").then((res) => {
         console.log(res.data);
-        this.grouptableData = res.data.data;
+        this.grouptableData = res.data.grouptableData;
       });
     },
   },
-  created(){
-  	this.download_grouplist();
-  }
+  filters: {
+    ellipsis: function (value) {
+      if (!value) return "";
+      if (value.length > 30) {
+        return value.slice(0, 30) + "...";
+      }
+      return value;
+    },
+  },
+  created() {
+    this.download_grouplist();
+  },
+  mounted: function () {
+    // 高度调整
+    this.elTableHeight = $(".booklist").height();
+  },
 };
 </script>
 
 <!--
+			{
+				grouppicture_link: "",
+				groupname: "groupazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+				group_link: "/",
+				group_size: 10000000000,
+			},
 {
 				grouppicture_link:'/',
 				groupname:'groupazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz',
