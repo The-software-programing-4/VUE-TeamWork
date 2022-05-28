afc52b644fcf7aef63e2cd409e7b0a707e45c6bd
Zihao Jing <1971008968@qq.com>
2022-05-28 22:22:35 +0800

    coparate

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -5,7 +5,8 @@ import store from './store'
 import ElementUI from 'element-ui'
 import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css';
-axios.defaults.withCredentials = true;
+
+axios.defaults.withCredentials = true 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.prototype.$axios = axios
