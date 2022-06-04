928c70c5c5a3eb17bf22edcb8b7454972a119158
Ivine11 <i2506851474@163.com>
2022-06-04 22:35:17 +0800

    pry6/4

--- vue-project/src/components/pry_part/topics.vue
+++ vue-project/src/components/pry_part/topics.vue
@@ -13,7 +13,7 @@
                 <el-button style="float: right; padding: 3px 0" type="text" @click="clickMv(topic.tid, 2)">加入话题</el-button>
             </div>
             <div class="text item">
-               话题名称{{topic.introduction}}，第{{currentPage}}页
+               话题名称：{{topic.introduction}}，第{{currentPage}}页
             </div>
             </el-card>
         </div>
