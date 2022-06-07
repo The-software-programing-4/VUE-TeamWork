13d43f475fe7e2b26ab34da728d6b00211350a20
Ivine11 <i2506851474@163.com>
2022-06-07 16:48:06 +0800

    topic change

--- vue-project/src/components/pry_part/topics.vue
+++ vue-project/src/components/pry_part/topics.vue
@@ -8,13 +8,15 @@
           (currentPage - 1) * pageSize,
           currentPage * pageSize
         )' >
-            <div slot="header" class="clearfix">
-                <span>话题卡片</span>
+            <div slot="header" class="clearfix" >
+                <el-badge :value="topic.focus" class="item" type="warning" style="float:left;height: 10px;">
+                <el-button size="small">关注</el-button>
+                </el-badge>
                 <el-button style="float: right; padding: 3px 0" type="text" @click="clickMv(topic.tid, 2)">加入话题</el-button>
             </div>
-            <div class="text item">
-               话题名称：{{topic.introduction}}，第{{currentPage}}页
-            </div>
+            <p class="text item" style="font-size:12px; overflow:hidden; text-overflow: ellipsis;white-space:nowrap; -webkit-line-clamp:2;">
+               {{topic.introduction}}
+            </p>
             </el-card>
         </div>
         <div class="pages-div">
@@ -33,25 +35,20 @@
         <div id="crossLine2">我关注的话题的动态</div>
     <el-divider class="line"><i class="el-icon-sunny"></i></el-divider>
     <!-- 话题展示 -->
-        <div id="img-show" v-for="topic in topic1List">
-            <div class="item-root">
-                    <img :src="topic.src" alt=""  @click="clickMv(topic.id, 2)">
+        <div class="topicComment">
+        <ul>
+          <li v-for="(item, index) in forums" class="commentItem">
+            <div class="commentNav">
+              <a href="#" class="commentUser">{{item.username}}</a>
+              <span class="commentThumb">{{item.thumb}}
+                <a @click="thumb(index)">点赞</a>
+              </span>
+              <span class="commentDate">{{item.day}}</span>
             </div>
-            <div class="detail">
-                <div>
-                    <button @click="clickMv(topic.id, 2)" id="nameButton">话题名字：{{topic.name}}</button>
-                </div>
-                <div>
-                    <p>话题简介：
-                        简介：123456789asdfghj从那时的擦的撒谎曾经我饿了能吃三餐都是你出生到成年的赛道你问
-                        简介：123456789asdfghj从那时的擦的撒谎曾经我饿了能吃三餐都是你出生到成年的赛道你问
-                        简介：123456789asdfghj从那时的擦的撒谎曾经我饿了能吃三餐都是你出生到成年的赛道你问
-                        简介：123456789asdfghj从那时的擦的撒谎曾经我饿了能吃三餐都是你出生到成年的赛道你问
-                        简介：123456789asdfghj从那时的擦的撒谎曾经我饿了能吃三餐都是你出生到成年的赛道你问
-                    </p>
-                </div>
-            </div>
-        </div>
+            <div>{{item.content}}</div>
+          </li>
+        </ul>
+      </div>
     </div>
 
 </template>
@@ -64,51 +61,23 @@ export default {
             currentPage: 1,
             pageSize: 6,
             topicList:[],
-            // 轮播图使用的图片列表
-            topic1List:[
-                {id:1,
-    src:require('./images/one.jpg'),
-                name:"pic1",
-                score:1},
-                {id:2,
-    src:require('./images/back8.jpg'),
-                name:"pic2",
-                score:2},
-                {id:3,
-    src:require('./images/back9.jpg'),
-                name:"pic3",
-                score:3},
-                 {id:5,
-        src:require('./images/one.jpg'),
-                name:"pic1",
-                score:1},
-                {id:7,
-    src:require('./images/back4.jpg'),
-                name:"pic2",
-                score:2},
-                {
-                id:8,
-    src:require('./images/back6.jpg'),
-                name:"pic3",
-                score:3},
-                {id:9,
-    src:require('./images/back5.jpg'),
-                name:"pic1",
-                score:1},
-                {id:11,
-    src:require('./images/back1.jpg'),
-                name:"pic2",
-                score:2},
-                {id:13,
-    src:require('./images/one.jpg'),
-                name:"pic3",
-                score:3},
-            //imagebox是assets下一个放图片的文件夹
-            ],
             str:'1',
             tid0:1,
             try:[{len:3}, {pid:2}, {pid:3}],
             tid:[],//查找得到该用户关注的tid
+            forms:[{
+          id: 1,//帖子id
+          username: 'ando',//发布者姓名
+          topicname:'abc',//所属话题名字
+          uid:"创建人名字",
+          tid:"话题名字",
+          content: '民国时期的经济环境居然已经如此丰富多彩了，什么商业竞争之类的应有尽有。在克劳对于中国人的一些总结的确实一针见血，像要面子这种，不愧是在中国生活这么久的人。整体写作风格偏幽默，虽然实际当时中国人民的生活并没有克劳所描写的那么乐观，但在当时已经算超前的了。',
+          thumb: 0,//点赞数量
+          // thumbId:[1, 2, 3],//存储点赞用户id
+          day: '2022.5.21 15:36:01', 
+          imgList:["src", "src", "src"]
+
+        },]
         }
     },
     methods:{
@@ -240,11 +209,14 @@ export default {
   }
 
   .box-card {
-    width: 27%;
+    width: 45%;
     float: left;
-    height: 40%;
+    height: 30%;
     margin-bottom:5% ;
     margin-right:3% ;
+    background:url("./images/back4.jpg");
+    background-size: 100%, 100%;
+    color: #ffffff;
   }
   /* card样式结束 */
 .topic-page{
